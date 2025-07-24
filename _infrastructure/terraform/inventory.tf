a90df4dfc84357e50457820eaa54a261e5cec468
Jonathan Math <jonathanamath@gmail.com>
2025-07-24 10:06:15 -0400

    add other host group names to ansible inventory so deployments can be dynamic

--- _infrastructure/terraform/inventory.tf
+++ _infrastructure/terraform/inventory.tf
@@ -1,8 +1,11 @@
 resource "local_file" "ansible_inventory" {
   filename = "${path.module}/../../inventory.ini"
   content  = <<-EOT
-  [portfolio]
-  ${data.terraform_remote_state.web.outputs.ec2_public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=_infrastructure/terraform/stacks/web/${local.ssh_key_name}
+  [logging_server]
+${local.sidecar_destination_ip} ansible_user=ubuntu ansible_ssh_private_key_file=${var.ssh_key_path}
+  
+  [logging_sidecar]
+    ${local.sidecar_destination_ip} ansible_user=ubuntu ansible_ssh_private_key_file=${var.ssh_key_path}
   EOT
 
 }
