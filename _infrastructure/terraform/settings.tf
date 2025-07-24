a8165777ba5a866266ccc2864f31e30f85b84aae
Jonathan Math <jonathanamath@gmail.com>
2025-07-24 10:05:46 -0400

    add github provider

--- _infrastructure/terraform/settings.tf
+++ _infrastructure/terraform/settings.tf
@@ -2,6 +2,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
+provider "github" {
+  token = var.github_token
+}
 
 terraform {
   backend "s3" {
---
+++ _infrastructure/terraform/variables.tf
@@ -0,0 +1,48 @@
+variable "ssh_key_path" {
+  description = "Path to the SSH private key used for connecting to the EC2 instances."
+  type        = string
+  default     = "~/.ssh/github_id_rsa"
+}
+
+variable "github_thumbprint" {
+  type    = string
+  default = "6938fd4d98bab03faadb97b34396831e3780aea1"
+}
+
+variable "github_token" {
+  description = "GitHub personal access token"
+  type        = string
+}
+
+variable "github_owner" {
+  description = "GitHub organization or user that owns the repository"
+  type        = string
+  default     = "jmath1"
+}
+
+variable "github_repository" {
+  description = "GitHub repository name"
+  type        = string
+  default     = "LogForge"
+}
+
+variable "ecr_registry" {
+  description = "Amazon ECR registry URL"
+  type        = string
+}
+
+variable "ecr_repository" {
+  description = "Amazon ECR repository name"
+  type        = string
+  default     = "logforge"
+}
+
+variable "aws_region" {
+  description = "AWS region for the ECR repository"
+  type        = string
+}
+
+variable "aws_account_number" {
+  description = "AWS account number"
+  type        = string
+}
